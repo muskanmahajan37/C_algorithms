e971b02651ca3c4d8d03748b2d8bc2e531d102fd
Walczy <60795188+walczy@users.noreply.github.com>
2020-12-04 21:43:20 +0100

    Update BubbleSort.c
    Sort Numbers from smaller to the bigger .

--- BubbleSort.c
+++ BubbleSort.c
@@ -1,1 +1,39 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
 
+void BubbleSort(int array[],int size);
+void Swap(int *x,int *y);
+
+int main(){
+    enum{size =10};
+    int array[size]={3,91,11,52,7,64,71,34,64,42};
+    BubbleSort(array,size);
+    for (int d=0;d<size;d++)
+    {
+      printf ("%d\n",array[d]);
+    }
+}
+
+
+void BubbleSort(int array[],int size)
+{
+    for (int i=0;i<size-1;i++)
+    {
+        for (int k=0;k<size;k++)
+        {
+            if (array[k]>array[k+1])
+            {
+                Swap(&array[k],&array[k+1]);
+            }
+        }
+
+    }
+}
+
+void Swap(int *x,int *y){
+    int temp=*x;
+    *x=*y;
+    *y = temp;
+}
+//by walczy
